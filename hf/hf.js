e88b41ca10299612e825106254ae9bd3dc146b55
Hyunykai <hyuny0603@naver.com>
2024-04-06 16:47:14 +0900

    header

---
+++ hf/hf.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => document.getElementById('header_hf').innerHTML = data);
+
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => document.getElementById('footer_hf').innerHTML = data);
+});
