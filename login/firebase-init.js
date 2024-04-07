cbf14e2879ead405279294f09a6770c2fba9b726
Hyunykai <hyuny0603@naver.com>
2024-04-07 16:52:11 +0900

    api key add

--- login/firebase-init.js
+++ login/firebase-init.js
@@ -1,11 +1,12 @@
 // Firebase 프로젝트 설정 정보
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAnf7jMvqle6VxKiFZkdC5yVsGE-7ZWbSg",
+    authDomain: "dutdut-fa20a.firebaseapp.com",
+    projectId: "dutdut-fa20a",
+    storageBucket: "dutdut-fa20a.appspot.com",
+    messagingSenderId: "797210029917",
+    appId: "1:797210029917:web:e9b8b330a7707199d082b4",
+    measurementId: "G-Q9MY1LQQS3"
 };
 
 // Firebase 초기화
