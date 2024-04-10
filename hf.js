23e69db7cb5da466eaa5f724891e78113e4d09f7
Hyunykai <hyuny0603@naver.com>
2024-04-10 14:02:37 +0900

    headfoot

--- hf.js
+++ hf.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('header.html')
+    fetch('https://dutdut.site/headfoot/header.html')
         .then(response => response.text())
         .then(data => document.getElementById('header_hf').innerHTML = data);
 
-    fetch('footer.html')
+    fetch('https://dutdut.site/headfoot/footer.html')
         .then(response => response.text())
         .then(data => document.getElementById('footer_hf').innerHTML = data);
 });
