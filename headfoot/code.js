9b8ab3871d1bff74aea674b257429f6c0bc369d9
Hyunykai <hyuny0603@naver.com>
2024-04-13 15:08:25 +0900

    로그인 상태 확인

--- headfoot/code.js
+++ headfoot/code.js
@@ -18,13 +18,14 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 document.addEventListener('DOMContentLoaded', () => {
-    const auth = getAuth();
+    // 이제 `auth` 변수는 이미 초기화된 인증 객체를 참조합니다.
     onAuthStateChanged(auth, (user) => {
         const loginButton = document.getElementById('logbt');
         if (user) {
-            console.log("로그인상태입니다.")
+            console.log("로그인상태입니다.");
             loginButton.classList.add('hidden'); // 로그인 상태일 때 버튼 숨기기
         } else {
+            console.log("로그아웃상태입니다.");
             loginButton.classList.remove('hidden'); // 로그아웃 상태일 때 버튼 표시
         }
     });
